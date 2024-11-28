a250ea179d63a94d1ebe4e1191c9b8bd877c2c97
Maxim <besstrakhov.m@gmail.com>
2024-11-28 09:57:29 +0200

    add Wrapper

---
+++ src/hoc/SWrapper.tsx
@@ -0,0 +1,24 @@
+import {characters, defaultHero} from "../utils/constants.ts";
+import {useParams} from "react-router-dom";
+import {useContext, useEffect} from "react";
+import {SWContext} from "../utils/context.ts";
+import ErrorPage from "../components/ErrorPage.tsx";
+
+export const SWrapper=WrappedComponent=>props=>{
+    const {heroId = defaultHero} = useParams();
+    const {changeHero} = useContext(SWContext);
+
+    useEffect(() => {
+        if (!characters[heroId]) {
+            return;
+        }
+        changeHero(heroId);
+    }, [heroId])
+
+    return characters[heroId] ? (
+        <div>
+            <WrappedComponent {...props}/>
+        </div>
+    )
+    :<ErrorPage/>
+}
